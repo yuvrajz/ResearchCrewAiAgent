c3d60378446bcaf12035c694ad9a9af48c2f6bc3
Yuvraj Zaware <yuvismartwork@gmail.com>
2025-07-06 16:58:12 +0530

    use HuggingFaceEndpoint

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from langchain_community.llms import HuggingFaceEndpoint
 
 def load_mistral():
-    return HuggingFaceHub(
+    return HuggingFaceEndpoint(
         repo_id="google/flan-t5-base",
         model_kwargs={"temperature": 0.7, "max_new_tokens": 512}
     )
